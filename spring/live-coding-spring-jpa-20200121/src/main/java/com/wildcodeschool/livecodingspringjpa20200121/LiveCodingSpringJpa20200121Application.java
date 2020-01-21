268b60fdf5e3e00a7e793cb2bbda68b5251abf17
Markus Schneider <markus@beefcafe.de>
2020-01-21 22:33:38 +0100

    Fixed name live-coding-spring-jpa-20200121

---
+++ spring/live-coding-spring-jpa-20200121/src/main/java/com/wildcodeschool/livecodingspringjpa20200121/LiveCodingSpringJpa20200121Application.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.livecodingspringjpa20200121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveCodingSpringJpa20200121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveCodingSpringJpa20200121Application.class, args);
+	}
+
+}
---
+++ spring/live-coding-spring-jpa-20200121/src/test/java/com/wildcodeschool/livecodingspringjpa20200121/LiveCodingSpringJpa2020121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.livecodingspringjpa20200121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCodingSpringJpa20200121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
